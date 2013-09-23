a4ce5dd0e20cf2f45d9e6e89f7406d9d718d341c
Chris <christind@hotmail.com>
2013-09-23 14:22:35 +0200

    - Added First-Person-Controller to the scene and mounted the main camera on the controller

---
+++ Assets/Scripts/Movement/FirstPersonController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(CharacterController))]
+public class FirstPersonController : MonoBehaviour {
+	
+	public float movementspeed = 4.0f;
+	public float mousesense = 2.0f;
+	public float updownRange = 60.0f;
+	
+	float verticalRotation = 0;
+	
+	CharacterController cc;
+	// Use this for initialization
+	void Start () 
+	{
+		Screen.lockCursor = true; // makes cursor invisible
+		cc = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{	
+		//Rotataion
+		
+		float rotLeftRight = Input.GetAxis("Mouse X") * mousesense;
+		transform.Rotate(0,rotLeftRight,0);
+		
+		verticalRotation -= Input.GetAxis("Mouse Y") * mousesense;
+		verticalRotation = Mathf.Clamp(verticalRotation, -updownRange, updownRange);
+		Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0 , 0);
+		
+		//Movement
+		
+		float forwardSpeed = Input.GetAxis("Vertical") * movementspeed;
+		float sideSpeed = Input.GetAxis("Horizontal") * movementspeed;
+		
+		Vector3 speed = new Vector3(sideSpeed,0,forwardSpeed);
+		
+		speed = transform.rotation * speed;
+		
+		cc.Move(speed * Time.deltaTime);
+	}
+}
