c247cd16325c18fd45c48d37e99e4f1b5910e706
Høgni Juul <hognijuul@gmail.com>
2013-10-09 12:45:20 +0000

    Start GUI - Added

---
+++ Assets/Scripts/GUIScript/MenuObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuObject : MonoBehaviour {
+	
+	public bool isQuit = false;
+	
+	
+	void OnMouseEnter()
+	{
+		
+		this.renderer.material.color = Color.green;
+	}
+	
+	void OnMouseExit()
+	{
+		renderer.material.color = Color.red;
+	}
+	
+	void OnMouseDown()
+	{
+		if (isQuit) 
+		{
+			//Ved ikke hvorfor den ikke virker!!!
+			Application.Quit();
+		}
+		else
+		{
+			Application.LoadLevel(1);
+		}
+	}
+}
