e0adcafda7bd343773ebdb9f0264fe00288c30a2
TokeOlsen <saxit_devil@hotmail.com>
2013-11-29 08:37:34 +0100

    Bugfixing

---
+++ Assets/Scripts/OpenFreezer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenFreezer : MonoBehaviour {
+	
+	public bool isOpen = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnMouseDown()
+	{
+		if(isOpen == false)
+		{
+			PlayForwards();
+			isOpen = true;
+		}
+		else
+		{
+			PlayBackwards();
+			isOpen = false;
+		}
+	}
+	
+	void PlayForwards()
+	{
+		animation["OpenFreezer"].speed = 1.0f;
+		animation.Play("OpenFreezer");
+	}
+	
+	void PlayBackwards()
+	{
+		animation["OpenFreezer"].speed = -1.0f;
+		animation["OpenFreezer"].time = animation["OpenFreezer"].length;
+		animation.Play("OpenFreezer");
+	}
+}
---
+++ Assets/Scripts/OpenFridge.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenFridge : MonoBehaviour {
+	
+	public bool isOpen = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnMouseDown()
+	{
+		if(isOpen == false)
+		{
+			PlayForwards();
+			isOpen = true;
+		}
+		else
+		{
+			PlayBackwards();
+			isOpen = false;
+		}
+	}
+	
+	void PlayForwards()
+	{
+		animation["OpenFridge"].speed = 1.0f;
+		animation.Play("OpenFridge");
+	}
+	
+	void PlayBackwards()
+	{
+		animation["OpenFridge"].speed = -1.0f;
+		animation["OpenFridge"].time = animation["OpenFridge"].length;
+		animation.Play("OpenFridge");
+	}
+}
