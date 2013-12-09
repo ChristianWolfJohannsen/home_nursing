0f4c927fc74e82ee28408b1968690ee92461e84f
TokeOlsen <saxit_devil@hotmail.com>
2013-12-09 21:05:47 +0100

    Dumping everything! (working here)

---
+++ Assets/Scripts/OpenFreezerDoor.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenFreezerDoor : MonoBehaviour
+{
+    bool isDoorOpen = false;
+    public AnimationClip animationClip;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void OnMouseDown()
+    {
+        Debug.Log("You clicked freezer door!");
+        if (Input.GetMouseButtonDown(0) && isDoorOpen == false)
+        {
+            Debug.Log("It plays the opening animation!!");
+            PlayForwards();
+            isDoorOpen = true;
+
+        }
+        else if (Input.GetMouseButtonDown(0) && isDoorOpen == true)
+        {
+            Debug.Log("It plays the closing animation!");
+            PlayBackwards();
+            isDoorOpen = false;
+        }
+    }
+
+
+
+    void PlayForwards()
+    {
+        animation["OpenFreezer"].speed = 1.0f;
+        animation.Play("OpenFreezer");
+    }
+
+    void PlayBackwards()
+    {
+        animation["OpenFreezer"].speed = -1.0f;
+        animation["OpenFreezer"].time = animation["OpenFreezer"].length;
+        animation.Play("OpenFreezer");
+    }
+}
--- Assets/Scripts/OpenFridge.cs
+++ Assets/Scripts/OpenFridge.cs
@@ -1,43 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class OpenFridge : MonoBehaviour {
-	
-	public bool isOpen = false;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-	
-	void OnMouseDown()
-	{
-		if(isOpen == false)
-		{
-			PlayForwards();
-			isOpen = true;
-		}
-		else
-		{
-			PlayBackwards();
-			isOpen = false;
-		}
-	}
-	
-	void PlayForwards()
-	{
-		animation["OpenFridge"].speed = 1.0f;
-		animation.Play("OpenFridge");
-	}
-	
-	void PlayBackwards()
-	{
-		animation["OpenFridge"].speed = -1.0f;
-		animation["OpenFridge"].time = animation["OpenFridge"].length;
-		animation.Play("OpenFridge");
-	}
+public class OpenFridge : MonoBehaviour
+{
+    bool isFridgeOpen = false;
+    public AnimationClip animationClip;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void OnMouseDown()
+    {
+        Debug.Log("You clicked fridge door");
+        if (Input.GetMouseButtonDown(0) && isFridgeOpen == false)
+        {
+            Debug.Log("It plays the opening animation!!");
+            PlayForwardsFridge();
+            isFridgeOpen = true;
+
+        }
+        else if (Input.GetMouseButtonDown(0) && isFridgeOpen == true)
+        {
+            Debug.Log("It plays the closing animation!");
+            PlayBackwardsFridge();
+            isFridgeOpen = false;
+        }
+    }
+
+
+
+    void PlayForwardsFridge()
+    {
+        animation["OpenFridge"].speed = 1.0f;
+        animation.Play("OpenFridge");
+    }
+
+    void PlayBackwardsFridge()
+    {
+        animation["OpenFridge"].speed = -1.0f;
+        animation["OpenFridge"].time = animation["OpenFridge"].length;
+        animation.Play("OpenFridge");
+    }
 }
