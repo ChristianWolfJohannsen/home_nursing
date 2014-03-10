6aa73d6b8fa632b8e71faaec098a4093e36bc3dc
Michael Dolatko <sayba_killer@hotmail.com>
2014-03-10 22:25:08 +0100

    Skybox

---
+++ Assets/JokerNavigator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JokerNavigator : MonoBehaviour {
+
+	public Transform target;
+	private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () 
+	{
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		agent.SetDestination (target.position);
+	}
+}
