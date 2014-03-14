9ac1e22b6fad5ecb6acfb352b22bd8dcb814ff82
Michael Dolatko <sayba_killer@hotmail.com>
2014-03-14 11:43:13 +0100

    no message

--- Assets/JokerNavigator.cs
+++ Assets/JokerNavigator.cs
@@ -1,20 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class JokerNavigator : MonoBehaviour {
 
-	public Transform target;
-	private NavMeshAgent agent;
+	public float patrolWaitTime = 1f;
+	public Transform[] patrolWayPoints;
+
+	private NavMeshAgent nav;
+	private float patrolTimer;
+	private int wayPointIndex;
 
 	// Use this for initialization
 	void Start () 
 	{
-		agent = GetComponent<NavMeshAgent> ();
+		nav = GetComponent<NavMeshAgent> ();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		agent.SetDestination (target.position);
+		NextRandomTarget ();
+	}
+
+	void NextRandomTarget()
+	{
+		if (nav.remainingDistance < nav.stoppingDistance) 
+		{
+				if(wayPointIndex == patrolWayPoints.Length - 1)
+					wayPointIndex = 0;
+				else
+					wayPointIndex++;
+
+					nav.destination = patrolWayPoints[wayPointIndex].position;
+		}
 	}
 }
