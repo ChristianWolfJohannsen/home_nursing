020118f808d4741195bc1b83055b039ca233cc44
Michael Dolatko <sayba_killer@hotmail.com>
2014-03-19 12:20:05 +0100

    Joker and Harley stops walking when the player approaches them.

--- Assets/JokerNavigator.cs
+++ Assets/JokerNavigator.cs
@@ -3,14 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class JokerNavigator : MonoBehaviour {
-
+	
 	public float patrolWaitTime = 1f;
 	public Transform[] patrolWayPoints;
-
+	
 	private NavMeshAgent nav;
 	private float patrolTimer;
 	private int wayPointIndex;
-
+	
 	// Use this for initialization
 	void Start () 
 	{
@@ -22,39 +22,64 @@ public class JokerNavigator : MonoBehaviour {
 	{
 		RandomPatrolling ();
 	}
-
+	
 	void Patrolling()
 	{
 		if (nav.remainingDistance < nav.stoppingDistance) {
-						patrolTimer += Time.deltaTime;
-
-						if (patrolTimer >= patrolWaitTime) {
-								if (wayPointIndex == patrolWayPoints.Length - 1)
+			patrolTimer += Time.deltaTime;
+			
+			if (patrolTimer >= patrolWaitTime) {
+				if (wayPointIndex == patrolWayPoints.Length - 1)
+					
+					wayPointIndex = 0;
+				else
+					
+					wayPointIndex++;
 				
-										wayPointIndex = 0;
-								else
+				patrolTimer = 0;
 				
-										wayPointIndex++;
-
-								patrolTimer = 0;
-
-						}
-				} else
-
-						patrolTimer = 0;
-
+			}
+		} else
+			
+			patrolTimer = 0;
+		
 		nav.destination = patrolWayPoints [wayPointIndex].position;
 	}
-
+	
 	void RandomPatrolling()
 	{
 		if (nav.remainingDistance < nav.stoppingDistance) 
 		{
 			patrolTimer += Time.deltaTime;
-
+			
 			int randomNumber = Random.Range(0, 5);
-
+			
 			nav.destination = patrolWayPoints[randomNumber].position;
+		}
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log ("Something has entered");
+		if (other.gameObject.tag == "Player")
+		{
+			Debug.Log("Player has entered");
+			
+			nav.speed = 0;
+			
+			Debug.Log("Joker should be still");
+		}
+	}
+	
+	void OnTriggerExit(Collider player)
+	{
+		if (player.gameObject.tag == "Player")
+		{
+			Debug.Log("Player has left Joker all alone... :(");
+			
+			nav.speed = 3.5f;
+			
+			Debug.Log("Joker should be still");
 		}
 	}
 }
