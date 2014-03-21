d7572508b6a2a858e3323d1043b0478d9ee47016
threefiddy <oeland13@hotmail.com>
2014-03-21 14:12:51 +0100

    Sinkwater

---
+++ Assets/HotwaterScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HotwaterScript : MonoBehaviour {
+
+	public bool isRunning = false;
+	private ParticleSystem Sinkwater;
+	public GameObject sinkWaterObj;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void OnMouseDown()
+	{
+		if (!isRunning) {
+			Debug.Log ("Sinkwater animation starting");
+			sinkWaterObj.particleSystem.Play();
+			isRunning = true;
+				}
+		else if (isRunning) {
+			Debug.Log("Sinkwater animation stopping");
+			sinkWaterObj.particleSystem.Stop();
+			isRunning = false;
+				}
+
+
+	}
+}
