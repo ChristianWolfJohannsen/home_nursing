59540668bba3b3dcaf84e3be04f200e001995013
Henrik Mikkelsen <hense-1990@sol.dk>
2014-03-23 21:52:53 +0100

    The fridge door and freezer door can now open and close

---
+++ Assets/Scripts/EventHandler/FridgeDoorHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FridgeDoorHandler : MonoBehaviour 
+{
+	private bool isOpen = false;
+	
+	void OnMouseDown()
+	{
+		if (!isOpen) 
+		{
+			isOpen = true;
+			animation.Play ("OpenFridge");
+		} 
+
+		else 
+		{
+			isOpen = false;
+			animation.Play ("CloseFridge");
+		}
+	}
+	
+}
