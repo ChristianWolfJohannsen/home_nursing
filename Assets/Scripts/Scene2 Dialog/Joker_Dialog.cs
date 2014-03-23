23e955ca282a9e9290faf420ef4c2649a8766f28
engboanders <engboanders@gmail.com>
2014-03-23 20:25:39 +0100

    Change a bit in Dialog. Made some bools that indicates if the player has accepted a quest or not. can now be used with items in the enviroment or with the wife dialog script, to further progress in the quest.

--- Assets/Scripts/Scene2 Dialog/Joker_Dialog.cs
+++ Assets/Scripts/Scene2 Dialog/Joker_Dialog.cs
@@ -6,9 +6,11 @@ public class Joker_Dialog : MonoBehaviour {
 	public string[] answerButtons;
 	public string[] questions;
 	bool DisplayDialog = false;
-	bool ActivateQuest = false;
-	bool QuestionAsked = false;
-	public int randomQuest;
+	public bool QuestionAsked = false;
+
+	public bool QuestAccepted = false;
+	public bool QuestAcceptedWife = false;
+	int randomQuest;
 	
 	
 	void Start () {
@@ -26,57 +28,55 @@ public class Joker_Dialog : MonoBehaviour {
 
 
 
-		if (DisplayDialog ) {
+		if (DisplayDialog && !QuestionAsked) {
+
 
 
 			GUILayout.Label (questions[randomQuest]);
-			Debug.Log("Quest asked");
+
 			
 
 			if(GUILayout.Button(answerButtons[0])){
+				Debug.Log("button clicked");
+				QuestionAsked = true;
 
-				DisplayDialog = false;
-				
 
 			}
-				Debug.Log("Answers given");
+				
+			if(GUILayout.Button (answerButtons[1])){
+
+				QuestionAsked = true;
+			}
 
 		}
+
+		if (DisplayDialog && QuestionAsked) {
+
+			GUILayout.Label (questions[3]);
+
+			if(GUILayout.Button(answerButtons[2])){
+				Debug.Log("button clicked");
+				DisplayDialog = false;
+				QuestionAsked = false;
+				QuestAccepted = true;
+				
+				
+			}
+			
+			if(GUILayout.Button (answerButtons[3])){
+				DisplayDialog = false;
+				QuestionAsked = false;
+				QuestAcceptedWife = true;
+			}
+		
+		}
+
+
 		GUILayout.EndArea ();
 
 	}
 
-//	void OnGUI(){
-//		GUILayout.BeginArea (new Rect(70, 60, 400, 400));
-//		if (DisplayDialog && !ActivateQuest) {
-//
-//						
-//			
-//			GUILayout.Label (questions [0]);
-//			GUILayout.Label (questions [1]);
-//			if (GUILayout.Button (answerButtons [0])) {
-//				ActivateQuest = true;
-//				
-//				DisplayDialog = false;
-//				
-//			}
-//			
-//			if (GUILayout.Button (answerButtons [1])) {
-//				DisplayDialog = false;	
-//			}
-//		}
-//		
-//		if(DisplayDialog && ActivateQuest){
-//			GUILayout.Label(questions[2]);
-//			if(GUILayout.Button(answerButtons[2])){
-//				DisplayDialog = false;
-//			}
-//		}
-//		
-//		GUILayout.EndArea ();
-//		
-//		
-//	}
+
 	
 	void OnTriggerEnter(){
 		DisplayDialog = true;
