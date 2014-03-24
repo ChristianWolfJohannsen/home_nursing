ff96e889baf194c38ac0d90ba7366af77f273c55
Michael Dolatko <sayba_killer@hotmail.com>
2014-03-24 11:12:37 +0100

    Radio now has music.

---
+++ Assets/RadioPLayer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RadioPLayer : MonoBehaviour {
+
+	private bool isRunning;
+
+	// Use this for initialization
+	void Start () 
+	{
+		audio.Play ();
+		isRunning = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown()
+	{
+		if (isRunning && Input.GetMouseButtonDown (0)) 
+		{
+			audio.Pause ();
+			isRunning = false;
+		} 
+		else if (!isRunning && Input.GetMouseButtonDown (0))
+		{
+			audio.Play();
+			isRunning = true;
+		}
+	}
+}
